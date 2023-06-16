3d621cee14954ef83524aa3b2afa57a509e526b6
Aisyahmsp <84187256+Aisyahmsp@users.noreply.github.com>
2023-06-16 12:09:57 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,89 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from numpy import array
+from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score, f1_score
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+import altair as alt
+from sklearn.utils.validation import joblib
+from sklearn.preprocessing import StandardScaler
+from PIL import Image
+
+# display
+st.set_page_config(page_title="Liver Prediction", page_icon='icon.jpg')
+
+primaryColor="#6eb52f"
+backgroundColor="yellow"
+secondaryBackgroundColor="#e0e0ef"
+textColor="#262730"
+font="sans serif"
+
+st.title("Sistem Prediksi Penyakit Liver")
+st.write("By: Aisyah Meta Sari Putri - 200411100031, dan Isnaini - 200411100038")
+st.write("Proyek Sains Data")
+st.write("========================================================================================")
+data, implementation = st.tabs(["Data", "Implementation"])
+
+df = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/00225/Indian%20Liver%20Patient%20Dataset%20(ILPD).csv")
+# Memberikan nama fitur pada tiap kolom dataset
+df.columns = ['Age', 'Gender', 'Total_Bilirubin', 'Direct_Bilirubin', 'Alkaline_Phosphotase',
+                'Alamine_Aminotransferase', 'Aspartate_Aminotransferase', 'Total_Protiens',
+                'Albumin', 'Albumin_and_Globulin_Ratio', 'Liver_disease']
+
+
+with data:
+    st.write("""# Tentang Dataset dan Aplikasi""")
+
+    st.write("Dataset yang digunakan adalah Indian Patient Liver dataset yang diambil dari https://archive.ics.uci.edu/ml/datasets/ILPD+(Indian+Liver+Patient+Dataset)")
+    st.write("Total datanya adalah 582 dengan data training 90% (523) dan data testing 10% (59)")
+
+with implementation:
+    st.write("# Implementation")
+    Age = st.number_input('Masukkan Umur Pasien')
+
+    # GENDER
+    gender = st.radio("Gender",('Male', 'Female'))
+    if gender == "Male":
+        Gender = 1
+    elif gender == "Female" :
+        Gender = 0
+
+    Total_Bilirubin = st.number_input('Masukkan Hasil Test Total_Bilirubin (Contoh : 10.9)')
+    Direct_Bilirubin = st.number_input('Masukkan Hasil Test Direct_Bilirubin (Contoh : 5.5)')
+    Alkaline_Phosphotase = st.number_input('Masukkan Hasil Test Alkaline_Phosphotase (Contoh : 699)')
+    Alamine_Aminotransferase = st.number_input('Masukkan Hasil Test Alamine_Aminotransferase (Contoh : 64)')
+    Aspartate_Aminotransferase = st.number_input('Masukkan Hasil Test Aspartate_Aminotransferase (Contoh : 100)')
+    Total_Protiens = st.number_input('Masukkan Hasil Test Total_Protiens (Contoh : 7.5)')
+    Albumin = st.number_input('Masukkan Hasil Test Albumin (Contoh : 3.2)')
+    Albumin_and_Globulin_Ratio = st.number_input('Masukkan Hasil Albumin_and_Globulin_Ratio (Contoh : 0.74)')
+   
+
+
+
+    def submit():
+        # input
+        inputs = np.array([[
+            Age, Gender, Total_Bilirubin, Direct_Bilirubin, Alkaline_Phosphotase,
+                Alamine_Aminotransferase, Aspartate_Aminotransferase, Total_Protiens,
+                Albumin, Albumin_and_Globulin_Ratio
+            ]])
+
+        if skor_akurasi > akurasiii:
+            model = joblib.load("svm_model.joblib")
+
+        elif akurasiii > skor_akurasi:
+            model = joblib.load("rf_model.joblib")
+
+        y_pred3 = model.predict(inputs)
+        st.write(f"Berdasarkan data yang Anda masukkan, maka anda diprediksi cenderung : {le.inverse_transform(y_pred3)[0]}")
+        st.write("0 = Tidak Terkena Liver")
+        st.write("1 = Terkena Liver")
+    all = st.button("Submit")
+    if all :
+        st.balloons()
+        submit()
